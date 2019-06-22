84ccecad74bc3ebec274b6dc5f10d60b61bb8d3f
Alex_Beng <599267065@qq.com>
2019-06-22 16:59:38 +0800

    pc.v done

---
+++ pc.v
@@ -0,0 +1,20 @@
+module pc( clk, rst, pc_wr, n_pc, pc );
+    input  clk;
+    input  rst;
+    input  pc_wr;
+    input  [31:0] n_pc;
+    output [31:0] pc;
+
+    reg [31:0] cnt;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            cnt <= 32'h0000_3000;
+        end
+        if (pc_wr) begin
+            cnt <= n_pc;
+        end
+    end
+
+    assign pc = cnt;
+endmodule
